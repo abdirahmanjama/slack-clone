0f48c195e44497e50e412ecc34e2904efd680277
Abdirahman <abdirahman.jamaaa@gmail.com>
2021-04-26 13:51:25 +0100

    Initialised Firebase Config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCp4QXXYdC8StTo1xzZndQV-Kn8SRn3AIY",
+  authDomain: "slack-clone-67704.firebaseapp.com",
+  projectId: "slack-clone-67704",
+  storageBucket: "slack-clone-67704.appspot.com",
+  messagingSenderId: "260479726538",
+  appId: "1:260479726538:web:56027f845cf5206d289533",
+};
